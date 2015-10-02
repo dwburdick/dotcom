99f388b197efa7b77e624530aa23efecba1dac7a
Dave Burdick <daveburdick@gmail.com>
2015-10-01 19:41:31 -0600

    Book reviews up to date

--- js/books.js
+++ js/books.js
@@ -9,6 +9,14 @@ $(function () {
   var context={
     "entries": [
       {
+        "title": "The Heart Goes Last",
+        "author": "Margaret Atwood",
+        "bookCode": "heart",
+        "imgAlt": "The Heart Goes Last book cover, depicting a man and a woman in prison-orange suits holding hands.",
+        "bookUrl": "http://www.denverpost.com/books/ci_28873826/review-heart-goes-last-by-margaret-atwood-dystopian",
+        "teaser": "In \"The Heart Goes Last,\" dystopia virtuoso Margaret Atwood turns her effortless world-building, deft humor and grim commentary on the depths of human hubris to the prison industrial complex, love and free will."
+      },
+      {
         "title": "Bright Lines",
         "author": "Tanwi Nandini Islam",
         "bookCode": "bright-lines",
@@ -33,40 +41,28 @@ $(function () {
         "teaser": "In Mia Couto's \"Confession of the Lioness,\" set in a put-upon Mozambican village, young Mariamar has had it rough: She has a despotic father, a detached mother, and her sister has just been killed by a lion. She wants out."
       },
       {
-        "title": "The Dark Forest",
-        "author": "Cixin Liu",
-        "bookCode": "dark-forest",
-        "maxWidth": "800",
-        "imgAlt": "The Dark Forest book cover with a foreboding image of a planet in space surrounded by mysterious shapes.",
-        "bookUrl": "http://www.denverpost.com/books/ci_28597429/review-dark-forest-gives-humanity-400-year-challenge",
-        "teaser": "\"The Dark Forest,\" the second installment in Chinese author Cixin Liu's \"Three-Body\" series, depicts a humanity trapped in indecision and argument over how to receive an invading alien civilization."
+        "title": "The Water Knife",
+        "author": "Paolo Bacigalupi",
+        "bookCode": "water-knife",
+        "imgAlt": "The Water Knife book cover with reflective blue cyberpunk-style lettering on a black background.",
+        "bookUrl": "http://www.denverpost.com/entertainment/ci_28254530/water-knife-review-waters-fighting-over",
+        "teaser": "It's the dark, action-packed story of a hired gun getting tangled up with a scrappy young girl and a semi-idealistic journalist in the catastrophically water-starved American Southwest of the not-so-distant future."
       },
       {
-        "title": "Bright Lines",
-        "author": "Tanwi Nandini Islam",
-        "bookCode": "bright-lines",
-        "maxWidth": "665",
-        "imgAlt": "Bright Lines book cover with an illustration of a silhouetted young girl with long hair riding a bicycle against bold lines radiating out from behind her in all colors of the spectrum.",
-        "bookUrl": "http://www.denverpost.com/books/ci_28597426/review-bright-lines-by-tanwi-nandini-islam-brooklyn",
-        "teaser": "A Brooklyn-by-way-of-Bangladesh \"Royal Tenenbaums.\" A pot-tinged, PTSD Muslim \"Sesame Street.\" With sex. Hallucinations, hijabs and handlebars on the always-busy Atlantic Avenue."
+        "title": "The Tusk That Did The Damage",
+        "author": "Tania James",
+        "bookCode": "tusk",
+        "imgAlt": "The Tusk That Did the Damage book cover with an elephant staring straight out at the viewer.",
+        "bookUrl": "http://www.denverpost.com/books/ci_27653826/review-tusk-that-did-damage-by-tania-james",
+        "teaser": "There is a myth-like inevitability about \"The Tusk That Did The Damage,\" which starts with Tania James' timeless first line: \"He would come to be called the Gravedigger.\""
       },
       {
-        "title": "Bright Lines",
-        "author": "Tanwi Nandini Islam",
-        "bookCode": "bright-lines",
-        "maxWidth": "665",
-        "imgAlt": "Bright Lines book cover with an illustration of a silhouetted young girl with long hair riding a bicycle against bold lines radiating out from behind her in all colors of the spectrum.",
-        "bookUrl": "http://www.denverpost.com/books/ci_28597426/review-bright-lines-by-tanwi-nandini-islam-brooklyn",
-        "teaser": "A Brooklyn-by-way-of-Bangladesh \"Royal Tenenbaums.\" A pot-tinged, PTSD Muslim \"Sesame Street.\" With sex. Hallucinations, hijabs and handlebars on the always-busy Atlantic Avenue."
-      },
-      {
-        "title": "The Dark Forest",
-        "author": "Cixin Liu",
-        "bookCode": "dark-forest",
-        "maxWidth": "800",
-        "imgAlt": "The Dark Forest book cover with a foreboding image of a planet in space surrounded by mysterious shapes.",
-        "bookUrl": "http://www.denverpost.com/books/ci_28597429/review-dark-forest-gives-humanity-400-year-challenge",
-        "teaser": "\"The Dark Forest,\" the second installment in Chinese author Cixin Liu's \"Three-Body\" series, depicts a humanity trapped in indecision and argument over how to receive an invading alien civilization."
+        "title": "Disgruntled",
+        "author": "Asali Solomon",
+        "bookCode": "disgruntled",
+        "imgAlt": "Disgruntled book cover with a young Black woman's face partially concealed by three stripes of color: pink, yellowish-green and light blue.",
+        "bookUrl": "http://www.denverpost.com/books/ci_27421661/asali-solomons-debut-novel-disgruntled-tackles-youth-and",
+        "teaser": "Kenya Curtis isn't yet disgruntled as a Philadelphia public school third-grader in 1984. But \"Disgruntled\" — the title of Asali Solomon's debut novel — is a fair description of everyone who's close to Kenya."
       },
     ]
 };
